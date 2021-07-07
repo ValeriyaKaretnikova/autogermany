95804cecd3d92d691b058a4d0125f18d3be0b76c
Karetnikova Valeriya <valeriya.karetnikova@gmail.com>
2021-07-07 10:41:11 -0600

    makes gallery mobile screen

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 window.addEventListener('load', function(e){
-    console.log(e);
+    // Toggle Navigation
     document.querySelector('#nav-icon').addEventListener('click', () => {
         document.querySelector('nav').classList.toggle('show-nav');
         document.querySelector('#nav-icon').classList.toggle('open');
         document.body.classList.toggle('lock');
+    });
+
+    // Sticky 
+    document.addEventListener('scroll', ()=>{
+
+        if (window.scrollY >20 ) {
+            document.querySelector('header').classList.add('scrolling');
+    
+        }
+        else {
+            document.querySelector('header').classList.remove('scrolling');
+    
+        }
     });
 })
